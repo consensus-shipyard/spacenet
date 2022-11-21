45f29eee7b825d523315af4db9de7a6d96b73e1b
Denis Kolegov <dnkolegov@gmail.com>
2022-11-21 18:07:03 +0100

    fix linting bugs

--- faucet/internal/db/db.go
+++ faucet/internal/db/db.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/ipfs/go-datastore"
+	"github.com/pkg/errors"
 
 	"github.com/filecoin-project/faucet/internal/data"
 	"github.com/filecoin-project/go-address"
@@ -29,10 +30,10 @@ func (db *Database) GetTotalInfo(ctx context.Context) (data.TotalInfo, error) {
 	var info data.TotalInfo
 
 	b, err := db.store.Get(ctx, totalInfoKey)
-	if err != nil && err != datastore.ErrNotFound {
+	if err != nil && !errors.Is(err, datastore.ErrNotFound) {
 		return data.TotalInfo{}, fmt.Errorf("failed to get total info: %w", err)
 	}
-	if err == datastore.ErrNotFound {
+	if errors.Is(err, datastore.ErrNotFound) {
 		return info, nil
 	}
 	if err := json.Unmarshal(b, &info); err != nil {
@@ -45,10 +46,10 @@ func (db *Database) GetAddrInfo(ctx context.Context, addr address.Address) (data
 	var info data.AddrInfo
 
 	b, err := db.store.Get(ctx, addrKey(addr))
-	if err != nil && err != datastore.ErrNotFound {
+	if err != nil && !errors.Is(err, datastore.ErrNotFound) {
 		return data.AddrInfo{}, fmt.Errorf("failed to get addr info: %w", err)
 	}
-	if err == datastore.ErrNotFound {
+	if errors.Is(err, datastore.ErrNotFound) {
 		return info, nil
 	}
 	if err := json.Unmarshal(b, &info); err != nil {
@@ -65,7 +66,7 @@ func (db *Database) UpdateAddrInfo(ctx context.Context, targetAddr address.Addre
 
 	err = db.store.Put(ctx, addrKey(targetAddr), bytes)
 	if err != nil {
-		return fmt.Errorf("failed to put addr info into db: %v", err)
+		return fmt.Errorf("failed to put addr info into db: %w", err)
 	}
 
 	return nil
@@ -79,7 +80,7 @@ func (db *Database) UpdateTotalInfo(ctx context.Context, info data.TotalInfo) er
 
 	err = db.store.Put(ctx, totalInfoKey, bytes)
 	if err != nil {
-		return fmt.Errorf("failed to put total info into db: %v", err)
+		return fmt.Errorf("failed to put total info into db: %w", err)
 	}
 
 	return nil
