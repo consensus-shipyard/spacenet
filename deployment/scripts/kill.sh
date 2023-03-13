2ed86c742a804baf158062a7684d2bfa6f6fcbe8
Matej Pavlovic <matopavlovic@gmail.com>
2023-03-13 13:55:20 +0100

    Ignore failures to kill processes during cleanup
    Not all processes are running on all machines,
    so killing some of them always fails when we run
    the generic killing script scripts/kill.sh.

--- deployment/scripts/kill.sh
+++ deployment/scripts/kill.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 killall eudico
-killall mir-validator
 killall spacenet-faucet
 tmux kill-server
 
 sleep 3
 
 killall -9 eudico
-killall -9 mir-validator
 killall -9 spacenet-faucet
 tmux kill-server
+
+# Some of the above commands inevitably fail, as not all processes are running on all machines.
+# This will prevent Ansible (through which this script is expected to be run) from complaining about it.
+true
