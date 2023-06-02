c34dead9417972e5021fa5709d00c328da8bc8e2
Denis Kolegov <dnkolegov@gmail.com>
2023-06-02 07:34:26 +0200

    fix gittag

--- deployment/scripts/start-health-monitoring.sh
+++ deployment/scripts/start-health-monitoring.sh
@@ -16,7 +16,7 @@ cd lotus || exit
 health_log_dir=~/spacenet-logs/health-$(date +%Y-%m-%d-%H-%M-%S_%Z)
 mkdir -p "$health_log_dir"
 
-gittag=$(git tag -l --sort=-creatordate | head -n 1 || echo "unk")
+gittag=$(git tag -l --sort=-creatordate | head -n 1)
 : ${gittag:="unk"}
 githash=$(git rev-parse --short=8 HEAD)
 flags="-X=github.com/filecoin-project/faucet/pkg/version.gittag=${gittag}"
